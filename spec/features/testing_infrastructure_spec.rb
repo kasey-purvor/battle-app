17a32bb3c1336084cb65516cacf24b7d1f44874d
Rorie Clarke <Rorie@MacBook-Pro.local>
2021-02-17 16:51:51 +0000

    Capybara working

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,4 +1,4 @@
-# require_relative 'app.rb'
+# require_relative '././spec_helper'
 
 
 feature 'Testing infrastructure' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,9 @@
 ENV['RACK_ENV']= 'test'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
-require 'rspec'
 require 'capybara/rspec'
+require 'rspec'
+
 
 Capybara.app = Battle
 
