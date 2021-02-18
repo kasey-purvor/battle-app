014c4d5500bc1edfecf42c2d9af3d4f14c3d0f0e
iainaitken <iain_aitken@yahoo.co.uk>
2021-02-18 17:20:11 +0000

    add player hp and damage feature

--- spec/features/attack_player_spec.rb
+++ spec/features/attack_player_spec.rb
@@ -2,6 +2,6 @@ feature 'attack player' do
   scenario 'attacking player 2' do
     sign_in_and_play
     click_link("attack player 2")
-    expect(page).to have_content "Fred has attacked Alex"
+    expect(page).to have_content "Fred has attacked Alex, and Alex's health is now 90"
   end
 end
